bd8cd6a847af27d7a4a1a8284bcf42436457431b
Tom <tmssmith@gmail.com>
2024-04-08 13:52:53 +0100

    If ball velocity = 0 treat as heading towards obs

--- pynball_rl/polygon_obstacle.py
+++ pynball_rl/polygon_obstacle.py
@@ -71,6 +71,8 @@ def heading_towards(ball: Ball, edge: list[Point]) -> bool:
         bool: _description_
     """
     v = [ball.get_center(), ball.get_velocity()]
+    if v[1].size() == 0.0:
+        return True
     e = [edge[0], edge[1].minus(edge[0])]
     if v[1].is_parallel_to(e[1]):
         return False
